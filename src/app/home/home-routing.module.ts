9d6834f512f4c1010c769e1eca25c8cbcde9849f
Parth Bhuva <parthbhuva126@gmail.com>
2023-11-23 18:25:37 +0530

    created navbar with lazy loading

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'home/child', loadChildren: () => import('./child/child.module').then(m => m.ChildModule) }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
