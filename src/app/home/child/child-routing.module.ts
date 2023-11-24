d36fa7ff7c0c7ff6b376c8fb0b8f92bbba390d18
Parth Bhuva <parthbhuva126@gmail.com>
2023-11-24 11:31:01 +0530

    child updated

--- src/app/home/child/child-routing.module.ts
+++ src/app/home/child/child-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChildComponent } from './child.component';
 
-const routes: Routes = [{ path: '', component: ChildComponent }];
+const routes: Routes = [
+  { path: '', component: ChildComponent },
+  { path: 'item1', loadChildren: () => import('./item1/item1.module').then(m => m.Item1Module) },
+  { path: 'item2', loadChildren: () => import('./item2/item2.module').then(m => m.Item2Module) },
+  { path: 'item3', loadChildren: () => import('./item3/item3.module').then(m => m.Item3Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'home/child', loadChildren: () => import('./child/child.module').then(m => m.ChildModule) }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'child', loadChildren: () => import('./child/child.module').then(m => m.ChildModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
