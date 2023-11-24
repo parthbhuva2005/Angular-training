5f284013bc7202b3d1e10a7d5ee56e82650bfd3a
Parth Bhuva <parthbhuva126@gmail.com>
2023-11-24 12:12:48 +0530

    day 1 last commit

--- src/app/home/child/child-routing.module.ts
+++ src/app/home/child/child-routing.module.ts
@@ -4,9 +4,6 @@ import { ChildComponent } from './child.component';
 
 const routes: Routes = [
   { path: '', component: ChildComponent },
-  { path: 'item1', loadChildren: () => import('./item1/item1.module').then(m => m.Item1Module) },
-  { path: 'item2', loadChildren: () => import('./item2/item2.module').then(m => m.Item2Module) },
-  { path: 'item3', loadChildren: () => import('./item3/item3.module').then(m => m.Item3Module) }
 ];
 
 @NgModule({
